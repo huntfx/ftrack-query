4aa19473a604b1acec422711a94ceaa664334d30
Peter Hunt <peter@huntfx.uk>
2022-05-03 18:10:03 +0100

    Fix wrong test cases
    Accidentally used assertTrue instead of assertEqual

--- tests/test_event.py
+++ tests/test_event.py
@@ -8,17 +8,17 @@ from ftrack_query import *
 
 class TestComparison(unittest.TestCase):
     def test_string(self):
-        self.assertTrue(event.a.b=='c', 'a.b="c"')
-        self.assertTrue(event.a.b!='c', 'a.b!="c"')
+        self.assertEqual(str(event.a.b == 'c'), 'a.b="c"')
+        self.assertEqual(str(event.a.b != 'c'), 'a.b!="c"')
 
     def test_int(self):
-        self.assertTrue(event.a.b==2, 'a.b=2')
+        self.assertEqual(str(event.a.b == 2), 'a.b=2')
 
     def test_and(self):
-        self.assertEqual(str(and_(event.a>0, b=5)), 'a>0 and b is 5')
+        self.assertEqual(str(and_(event.a > 0, b=5)), 'a>0 and b is 5')
 
     def test_or(self):
-        self.assertEqual(str(or_(event.a>0, b=5)), '(a>0 or b is 5)')
+        self.assertEqual(str(or_(event.a > 0, b=5)), '(a>0 or b is 5)')
 
     def test_contains_error(self):
         with self.assertRaises(TypeError):
